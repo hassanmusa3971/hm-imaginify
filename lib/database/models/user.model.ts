bd857955c26b1cfc5a42771e3ad9d406a9bc85d8
hassanmusa3971 <hassanmusa3971@gmail.com>
2024-10-03 22:28:52 -0700

    Initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,16 @@
+import { Document, model, models, Schema } from "mongoose";
+
+
+const UserSchema = new Schema({
+      clerk_id: { type: String, required: true, unique: true },
+      email: { type: String, required: true, unique: true },
+      username: { type: String, required: true, unique: true },
+      photo: { type: String, required: true },
+      firstName: { type: String },
+      lastName: { type: String},
+      plainId: { type: Number, default: 1 },
+      creditBlance: { type: Number, default: 10 },
+});
+
+const User = models.User || model("User", UserSchema);
+export default User;
