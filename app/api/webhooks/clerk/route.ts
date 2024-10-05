da8321442487da5c3440cb8a03cbb4d899d0aee9
hassanmusa3971 <hassanmusa3971@gmail.com>
2024-10-05 06:00:46 -0700

    Clerk and Webhooks fix two

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
+    const user:any = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
