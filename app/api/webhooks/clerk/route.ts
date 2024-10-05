0d43a1b7127355e0ed437a79717baa1fe39967f5
hassanmusa3971 <hassanmusa3971@gmail.com>
2024-10-05 06:06:06 -0700

    Clerk and Webhooks fix three

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -60,13 +60,13 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user:any = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
+    const user = {
+      clerkId: id!,
+      email: email_addresses[0].email_address!,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url!,
     };
 
     const newUser = await createUser(user);
@@ -88,10 +88,10 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
-      photo: image_url,
+      photo: image_url!,
     };
 
     const updatedUser = await updateUser(id, user);
